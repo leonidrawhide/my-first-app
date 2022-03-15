0a341cce63839508cd6f3bfb3c15550c67f507da
Leonid (notebook) <leonidrawhide@gmail.com>
2022-03-15 15:36:41 +0300

    relocated registration module

---
+++ src/app/registration.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RegistrationComponent } from './registration/registration.component';
+import { AppModule } from './app.module';
+
+
+@NgModule({
+  declarations: [
+    RegistrationComponent
+  ],
+  imports: [
+    AppModule
+  ]
+})
+export class RegistrationModule { }
