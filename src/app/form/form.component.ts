385ffff59dc694b6b4ba3002366b389dd0842810
Leonid (notebook) <leonidrawhide@gmail.com>
2022-03-24 14:49:34 +0300

    minor fixes

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -11,6 +11,13 @@ import { ValidateCyrillic } from '../../shared/cyrillic.validator';
   styleUrls: ['./form.component.scss']
 })
 export class FormComponent implements OnInit {  
+
+  constructor(private fb: FormBuilder) {
+    for (let i = 0; i < this.skillsBasic.length; i++) {
+      this.skills.push(this.fb.control(this.skillsBasic[i].name));
+    }
+  }
+
   form = this.fb.group({
     name: this.fb.group({
       firstName: ['', [Validators.required, ValidateCyrillic]],
@@ -27,26 +34,20 @@ export class FormComponent implements OnInit {
 
   addOnBlur = true;
   readonly separatorKeysCodes = [ENTER, COMMA] as const;
-  skillsCopy: Skill[] = [{name: 'Жизнерадостность'}, {name: 'Заинтересованность'}, {name: 'Интеллект'}];
+  skillsBasic: Skill[] = [{name: 'Жизнерадостность'}, {name: 'Заинтересованность'}, {name: 'Интеллект'}];
 
   add(event: MatChipInputEvent): void {
     const value = (event.value || '').trim();
 
-    // Add our fruit
     if (value) {
-      this.skillsCopy.push({name: value});
       this.skills.push(this.fb.control(value));
     }
 
-    // Clear the input value
     event.chipInput!.clear();
   }
 
-  remove(skill: Skill): void {
-    const index = this.skillsCopy.indexOf(skill);
-
+  remove(index: number): void {
     if (index >= 0) {
-      this.skillsCopy.splice(index, 1);
       this.skills.removeAt(index);
     }
   }
@@ -54,23 +55,13 @@ export class FormComponent implements OnInit {
   clearForm() {
     this.form.reset();
     this.skills.clear()
-    this.skillsCopy = [{name: 'Жизнерадостность'}, {name: 'Заинтересованность'}, {name: 'Интеллект'}];
-    for (let i = 0; i < this.skillsCopy.length; i++) {
-      this.skills.push(this.fb.control(this.skillsCopy[i].name));
-    }
-  }
 
-  constructor(private fb: FormBuilder, private element: ElementRef) {
-    for (let i = 0; i < this.skillsCopy.length; i++) {
-      this.skills.push(this.fb.control(this.skillsCopy[i].name));
+    for (let i = 0; i < this.skillsBasic.length; i++) {
+      this.skills.push(this.fb.control(this.skillsBasic[i].name));
     }
-    console.log(this.element)
-    // console.log(document.getElementById('skills-input'))
   }
 
   ngOnInit(): void {
-    console.log(this.skillsCopy)
-    console.log(this.skills)
   }
 
 }
